ec2cb69dfbb5fc49ce230237e3099623c7a0d36f
walaa111 <walaagamil18@gmail.com>
2018-02-13 15:32:58 +0200

    w

--- series_app_ads/www/js/index.js
+++ series_app_ads/www/js/index.js
@@ -67,8 +67,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-7251676025279948/2430262387',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7251676025279948/4077072896'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-7251676025279948/2193317023',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/6870928631'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
